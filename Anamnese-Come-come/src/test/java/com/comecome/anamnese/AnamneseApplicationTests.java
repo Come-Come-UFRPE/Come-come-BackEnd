c06595a07f11bb61a25566a97a7e6a89246c6e5f
JuanPabollo <pabollo61@gmail.com>
2025-11-02 22:10:03 -0300

    [fix] maven

---
+++ Anamnese-Come-come/src/test/java/com/comecome/anamnese/AnamneseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comecome.anamnese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnamneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
