ebdaec5f6976cdbe5260519fbb619d455bd0654e
Wellington Viana <osmcanal@gmail.com>
2025-11-16 14:51:41 -0300

    [fixes] merging conflicts

--- OFF-Come-come/src/main/java/com/comecome/openfoodfacts/service/ComparingService.java
+++ OFF-Come-come/src/main/java/com/comecome/openfoodfacts/service/ComparingService.java
@@ -2,11 +2,13 @@ package com.comecome.openfoodfacts.service;
 
 import com.comecome.openfoodfacts.dtos.AnamneseSearchDTO;
 import com.comecome.openfoodfacts.dtos.ComparePatchDTO;
+import com.comecome.openfoodfacts.dtos.UiFilterDto;
 import com.comecome.openfoodfacts.dtos.responseDtos.ComparingResponseDTO;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.Set;
 
 @Service
 public class ComparingService {
@@ -20,8 +22,9 @@ public class ComparingService {
     public Mono<ComparingResponseDTO> comparingResponse(ComparePatchDTO patchDTO){
         AnamneseSearchDTO dtoA = new AnamneseSearchDTO(patchDTO.userID(),patchDTO.idA());
         AnamneseSearchDTO dtoB = new AnamneseSearchDTO(patchDTO.userID(),patchDTO.idB());
-        Mono<Map> productA = openFoodFactsService.searchProducts(dtoA,"en:brazil",dtoA.getUserID());
-        Mono<Map> productB = openFoodFactsService.searchProducts(dtoB,"en:brazil",dtoB.getUserID());
+        UiFilterDto dto = new UiFilterDto(Set.of(),Set.of(), Set.of(), Set.of());
+        Mono<Map> productA = openFoodFactsService.searchProducts(dtoA,"en:brazil",dtoA.getUserID(), dto);
+        Mono<Map> productB = openFoodFactsService.searchProducts(dtoB,"en:brazil",dtoB.getUserID(), dto);
 
         return Mono.zip(productA, productB)
                 .map(tuple -> {
