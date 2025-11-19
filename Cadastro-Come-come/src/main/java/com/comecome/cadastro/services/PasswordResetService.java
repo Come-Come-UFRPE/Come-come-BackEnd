9bc3c718adf73340bd87fe3a4186a88e4df5a3d8
Wellington Viana <osmcanal@gmail.com>
2025-11-19 01:04:36 -0300

    [feature] add rabbitmq listeners and configurations

--- Cadastro-Come-come/src/main/java/com/comecome/cadastro/services/PasswordResetService.java
+++ Cadastro-Come-come/src/main/java/com/comecome/cadastro/services/PasswordResetService.java
@@ -1,9 +1,12 @@
 package com.comecome.cadastro.services;
 
+import com.comecome.cadastro.dtos.TokenDTO;
 import com.comecome.cadastro.models.PasswordResetToken;
 import com.comecome.cadastro.models.User;
 import com.comecome.cadastro.repositories.PasswordResetRepository;
 import com.comecome.cadastro.repositories.UserRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +20,15 @@ public class PasswordResetService {
     private final UserRepository userRepository;
     private final PasswordResetRepository passwordRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RabbitTemplate rabbitTemplate;
 
-    public PasswordResetService(UserRepository userRepository,PasswordResetRepository repository, PasswordEncoder passwordEncoder) {
+    private static final String fila = "send-emails";
+
+    public PasswordResetService(UserRepository userRepository, PasswordResetRepository repository, PasswordEncoder passwordEncoder, RabbitTemplate rabbitTemplate) {
         this.userRepository = userRepository;
         this.passwordRepository = repository;
         this.passwordEncoder = passwordEncoder;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
 
@@ -33,7 +40,11 @@ public class PasswordResetService {
 
         String otpCode = generateOTPToken();
 
+        TokenDTO sendDto = new TokenDTO(email,otpCode);
+
         persistNewToken(user, otpCode);
+
+        rabbitTemplate.convertAndSend("",fila,sendDto);
 
         return otpCode;
     }
@@ -87,18 +98,23 @@ public class PasswordResetService {
 
     }
 
+    //Função que vai atuar no reset da senha
     public void resetPassword(String email, String otp, String newPassword){
         var user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
         PasswordResetToken token = passwordRepository.findByUser(user).orElseThrow(() -> new RuntimeException("Token de usuário não encontrado!"));
 
         if (token.isExpired() || !passwordEncoder.matches(otp, token.getTokenHash())) {
             throw new RuntimeException("Token inválido ou expirado na etapa final!");
         }
 
-        user.setPassword(passwordEncoder.encode(newPassword));
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setPassword(encoder.encode(newPassword));
+
         userRepository.save(user);
 
-        // 4. AGORA SIM: Deleta o token (Missão cumprida)
+
+
         passwordRepository.delete(token);
 
     }
