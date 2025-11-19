2f601398e519259b47c785d562ca66084e1db024
Wellington Viana <osmcanal@gmail.com>
2025-11-19 01:04:36 -0300

    [feature] add rabbitmq listeners and configurations

--- Email-Come-come/src/main/java/com/comecome/email/controllers/EmailController.java
+++ Email-Come-come/src/main/java/com/comecome/email/controllers/EmailController.java
@@ -1,33 +1,25 @@
 package com.comecome.email.controllers;
 
+import com.comecome.email.services.EmailService;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @RestController
 public class EmailController {
 
-    private final JavaMailSender mailSender;
+    private final EmailService service;
 
-    public EmailController(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    public EmailController(EmailService service) {
+        this.service = service;
     }
 
-    @RequestMapping("/send-email")
-    public String sendEmail() {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("contatesteapify1@gmail.com");
-            message.setTo("contatesteapify1@gmail.com");
-            message.setSubject("Email Conta Corrente");
-            message.setText("Email Conta Corrente");
-
-            mailSender.send(message);
-            return "Teste";
-        } catch (Exception e) {
-            e.getMessage();
-            return "Error";
-        }
-    }
 }
