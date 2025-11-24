bb1edbe3aeb718899810b9ddde22b8cfb62e1a84
Wellington Viana <osmcanal@gmail.com>
2025-11-23 23:34:33 -0300

    [fix] little fix concurrency

--- Email-Come-come/src/main/java/com/comecome/email/config/RabbitConfig.java
+++ Email-Come-come/src/main/java/com/comecome/email/config/RabbitConfig.java
@@ -1,5 +1,6 @@
 package com.comecome.email.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
+    public static final String fila_email = "send-emails";
+
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue emailQueue() {
+
+        return new Queue(fila_email, true);
+
     }
 
 }
