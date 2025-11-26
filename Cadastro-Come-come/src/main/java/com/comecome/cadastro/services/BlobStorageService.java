7833659c9f3c86c7121d8654d91d0a22a7f4eea5
Wellington Viana <osmcanal@gmail.com>
2025-11-25 23:10:44 -0300

    [add] profile enhanced with azure blob storage

---
+++ Cadastro-Come-come/src/main/java/com/comecome/cadastro/services/BlobStorageService.java
@@ -0,0 +1,35 @@
+package com.comecome.cadastro.services;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class BlobStorageService {
+
+    private final BlobContainerClient blobContainerClient;
+
+    public BlobStorageService(BlobContainerClient blobContainerClient) {
+        this.blobContainerClient = blobContainerClient;
+    }
+
+    public String uploadFile(String fileName, InputStream data, long size){
+        BlobClient client = blobContainerClient.getBlobClient(fileName);
+        client.upload(data, size, true);
+
+        return client.getBlobUrl();
+    }
+
+    public void deleteProfilePicture(UUID userId) {
+        // Reconstrói o caminho do arquivo (baseado na lógica que definimos antes)
+        String path = String.format("users/%d/profile.jpg", userId);
+
+        BlobClient client = blobContainerClient.getBlobClient(path);
+
+        // Tenta deletar. Se não existir, não faz nada e segue a vida.
+        client.deleteIfExists();
+    }
+}
