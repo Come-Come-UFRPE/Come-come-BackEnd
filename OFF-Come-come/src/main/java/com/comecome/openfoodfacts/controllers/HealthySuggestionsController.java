49ff3bf02973c3c691f5e9c7d4b85159c8517ed5
uslipe <felipe.pereira.nave@gmail.com>
2025-12-07 14:26:07 -0300

    feat: open route on controller

---
+++ OFF-Come-come/src/main/java/com/comecome/openfoodfacts/controllers/HealthySuggestionsController.java
@@ -0,0 +1,36 @@
+package com.comecome.openfoodfacts.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.comecome.openfoodfacts.dtos.SuggestionsDTO;
+import com.comecome.openfoodfacts.models.Produto;
+import com.comecome.openfoodfacts.service.SuggestionsService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/foods")
+public class HealthySuggestionsController {
+
+    private final SuggestionsService suggestionsService;
+
+    public HealthySuggestionsController(SuggestionsService suggestionsService){
+        this.suggestionsService = suggestionsService;
+    }
+    
+    @PostMapping("/suggestions")
+    public ResponseEntity<List<Produto>> healthySuggestions(@RequestBody SuggestionsDTO sugestao) {
+        String query = sugestao.sugestao();
+        
+        List<Produto> response = suggestionsService.getSugestoes(query);
+
+        return ResponseEntity.ok(response);
+    }
+    
+}
